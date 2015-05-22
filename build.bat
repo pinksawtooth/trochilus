887ace808ca14c86fb9123570307d22483e2c686
floyd <floyd419@foxmail.com>
2015-05-22 11:09:58 +0800

    fix build bug

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ taskkill /f /im Generator.exe
 
 echo ¿½±´¿ØÖÆ¶ËÄÚÈİ
 copy /Y %cd%\server\bin\master.dll %cd%\bin\bin\master.dll 
+copy /Y %cd%\server\bin\midutils.dll %cd%\bin\bin\midutils.dll 
 copy /Y %cd%\server\bin\Trochilus.exe %cd%\bin\bin\Trochilus.exe
 copy /Y %cd%\client\bin\body.dll %cd%\bin\bin\modules\svtlogo.dll
 
