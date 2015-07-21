f5628504f0f6b8494d072fc903eab75946710ee9
floyd <floyd419@foxmail.com>
2015-07-21 16:48:58 +0800

    update clear.

--- clear.bat
+++ clear.bat
@@ -4,6 +4,6 @@ del C:\ods.log
 sc stop medialoader
 sc delete medialoader
 
-del %ALLUSERSPROFILE%\ser.dat
-del %ALLUSERSPROFILE%\shell.dll
-del %ALLUSERSPROFILE%\svtlogo.dat
+del %ALLUSERSPROFILE%\Medialoader\ser.dat
+del %ALLUSERSPROFILE%\Medialoader\shell.dll
+del %ALLUSERSPROFILE%\Medialoader\data.dat
